081ecc42936fa810861cfd9232c20b3af87ff868
Paalpe <37175836+Paalpe@users.noreply.github.com>
2024-01-02 13:47:06 +0100

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/paal-sorvik-pedersen/Notion-DoIt-5e72fe99af2d450aabba87364a24acba?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
